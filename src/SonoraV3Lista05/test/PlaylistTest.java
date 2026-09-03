15474dc13fb7f2da8e4df21e883c5f1c99ee3ffb
LorenzoMaiola <lorenzogiusepemaiola@gmail.com>
2026-09-03 13:18:04 -0300

    Plano de testes pl04

--- src/SonoraV3Lista05/test/PlaylistTest.java
+++ src/SonoraV3Lista05/test/PlaylistTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlaylistTest {
@@ -19,6 +21,8 @@ public class PlaylistTest {
         return new Musica("Titulo", "Artista", 120);
     }
 
+    //PL03
+    
     @DisplayName("Adicionar musica em playlist com espaco disponivel")
     @Test
     void playlistComEspacoDeveAdicionarMusica() {
@@ -50,4 +54,51 @@ public class PlaylistTest {
         Playlist playlist = new Playlist("Playlist", usuario);
         assertThrows(IllegalArgumentException.class, () -> playlist.adicionar(null));
     }
+
+
+    //PL04 - validar getNaPosicao()
+    
+    @DisplayName("Indice valido no meio da playlist devolve a musica certa")
+    @Test
+    void indiceValidoDevolveMusicaCerta(){
+        Musica musica1 = new Musica("Titulo", "artista", 120);
+        Musica musica2 = new Musica("Titulo", "artista", 120);
+        Musica musica3 = new Musica("Titulo", "artista", 120);
+
+        Playlist playlist = new Playlist("Playlist", usuario);
+        playlist.adicionar(musica1);
+        playlist.adicionar(musica2);
+        playlist.adicionar(musica3);
+        assertEquals(playlist.getNaPosicao(1), musica2);
+    }
+
+    @DisplayName("Indice negativo deve ser rejeitado")
+    @Test
+    void indiceNegativoDeveSerRejeitado(){
+        Playlist playlist = criarEPreencherPlaylist(3);
+        assertThrows(IndexOutOfBoundsException.class, () -> playlist.getNaPosicao(-1));
+    }
+
+    @DisplayName("Indice igual a quantidade")
+    @Test
+    void indiceIgualAQuantidadeDeveSerRejeitado(){
+        Playlist playlist = criarEPreencherPlaylist(3);
+        assertThrows(IndexOutOfBoundsException.class, () -> playlist.getNaPosicao(3));
+    }
+
+    @DisplayName("Índice maior que a quantidade")
+    @Test
+    void indiceMaiorQueAQuantidadeDeveSerRejeitada(){
+        Playlist playlist = criarEPreencherPlaylist(3);
+        assertThrows(IndexOutOfBoundsException.class, () -> playlist.getNaPosicao(10));
+    }
+
+    @DisplayName("Playlist vazia, qualquer indice é inválido")
+    @Test
+    void playlistVaziaNaoDeveAceitarNenhumIndice(){
+        Playlist playlist = new Playlist("nome", usuario);
+        assertThrows(IndexOutOfBoundsException.class, () -> playlist.getNaPosicao(0));
+    }
+
+    //PL05 - validar Playlist.removerNaPosicao()
 }
